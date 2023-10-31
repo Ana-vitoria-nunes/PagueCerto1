0212fffd404d4d6a881a7ac4ef71d3b62f325964
ana <ana.bernardes@zup.com.br>
2023-10-31 14:26:37 -0300

    Subindo classe pagamento e cliente

---
+++ src/main/java/org/example/core/useCase/payment/PaymentService.java
@@ -0,0 +1,102 @@
+package org.example.core.useCase.payment;
+
+import lombok.RequiredArgsConstructor;
+import org.example.core.domain.excecao.NoItemException;
+import org.example.core.domain.model.Card;
+import org.example.core.domain.model.Debts;
+import org.example.core.domain.model.Payment;
+import org.example.core.domain.model.Status;
+import org.example.core.domain.model.dto.requestDto.DebstRequestPayment;
+import org.example.core.domain.model.dto.requestDto.PaymentDto;
+import org.example.core.domain.model.dto.responseDto.ResponseDto;
+import org.example.core.port.CardRepository;
+import org.example.core.port.DebtsRepository;
+import org.example.core.port.PaymentRepository;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentService {
+      private final PaymentRepository paymentRepository;
+      private final DebtsRepository debtsRepository;
+      private final CardRepository cardRepository;
+      private double juros = 0;
+      private int parcela = 0;
+      // lembrar de adicionar a quantidade de parcelas
+    public Payment insertValueAcordingToDebts(DebstRequestPayment requestPayment, PaymentDto paymentDto) {
+
+        Debts debts = debtsRepository.findByExternalIdDebts(requestPayment.getIdexternoDebts()).orElseThrow(() ->
+                new NoItemException("Divida não encontrada"));
+
+        Card card = cardRepository.findByExternalIdCard(requestPayment.getIdexternoCard()).orElseThrow(() ->
+                new NoItemException("Cartão não encontrado"));
+
+
+        Payment paymentMapDto = new Payment();
+
+        switch (requestPayment.getEscolha()) {
+            case 1 -> {
+                juros = 0.05;
+                parcela = 2;
+            }
+            case 2 -> {
+                juros = 0.07; // Juros de 7% para 4 parcelas
+                parcela = 4;
+            }
+            case 3 -> {
+                juros = 0.09; // Juros de 9% para 6 parcelas
+                parcela = 6;
+            }
+            case 4 -> {
+                juros = 0.11; // Juros de 11% para 8 parcelas
+                parcela = 8;
+            }
+            case 5 -> {
+                juros = 0.13; // Juros de 13% para 10 parcelas
+                parcela = 10;
+            }
+            case 6 -> {
+                juros = 0.15; // Juros de 15% para 12 parcelas
+                parcela = 12;
+
+            }
+            default -> new ResponseDto("Opção inválida, tente novamente");
+        }
+        BigDecimal valorTotalEmprestimo = debts.getDebts().multiply(BigDecimal.ONE.add(BigDecimal.valueOf(juros)));
+        BigDecimal valorTotalParcela = valorTotalEmprestimo.divide(BigDecimal.valueOf(parcela), 2, BigDecimal.ROUND_HALF_UP);
+
+        paymentMapDto.generateAndSetExternalIdPayment();
+        paymentDto.setValorTotalParcela(valorTotalParcela);
+        paymentDto.setValorTotalEmprestimo(valorTotalEmprestimo);
+
+
+        paymentMapDto.setQuota(requestPayment.getEscolha());
+        paymentMapDto.setExternalIdCard(card);
+        paymentMapDto.setTotalLending(paymentDto.getValorTotalEmprestimo());
+        paymentMapDto.setTotalQuota(paymentDto.getValorTotalParcela());
+        paymentMapDto.setDayDebts(paymentMapDto.getDayDebts());
+
+
+        LocalDate dateToday = LocalDate.now();
+        LocalDate datePaymentChoose = requestPayment.getDatePaymentChoose();
+
+        if (dateToday.getMonthValue() != datePaymentChoose.getMonthValue()){
+            paymentMapDto.setStats(Status.PENDENTE);
+        } else {
+            paymentMapDto.setStats(Status.CONCLUIDO);
+        }
+        paymentMapDto.setDayMoth(requestPayment.getDatePaymentChoose());
+
+        paymentRepository.save(paymentMapDto);
+
+        debts.setExternalIdPayment(paymentMapDto);
+        debtsRepository.save(debts);
+
+        return paymentMapDto;
+    }
+
+}
+
