af141bd46b3113aebfafad507421baede2657872
Ana.Vitoria.Nunes <ana.nunes@zup.com.br>
2023-10-31 13:27:49 -0300

    Commit inicial

---
+++ src/main/java/org/example/SistemaPagueCerto.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@ComponentScan(basePackages = "org.example")
+public class SistemaPagueCerto {
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaPagueCerto.class, args);
+    }
+
+}
