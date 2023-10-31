7cab87e90c52f4a67ef062a7f7fc70fa30332cc4
Vitoria.Cerqueira <vitoria.cerqueira@zup.com.br>
2023-10-31 13:46:03 -0300

    subindo aplicação

---
+++ src/main/java/org/example/adapters/web/PaymentController.java
@@ -0,0 +1,31 @@
+package org.example.adapters.web;
+
+import lombok.RequiredArgsConstructor;
+import org.example.core.domain.model.dto.requestDto.DebstRequestPayment;
+import org.example.core.domain.model.dto.requestDto.PaymentDto;
+import org.example.core.domain.model.dto.responseDto.ResponseDto;
+import org.example.core.useCase.payment.PaymentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pagamento")
+@RequiredArgsConstructor
+public class PaymentController {
+   private final PaymentService paymentService;
+
+   @PostMapping
+    public ResponseEntity save (@RequestBody DebstRequestPayment debstRequestPayment, PaymentDto paymentDto ){
+       try {
+           paymentService.insertValueAcordingToDebts(debstRequestPayment,paymentDto);
+           return new ResponseEntity<>(new ResponseDto("Parabens você efetuou seu pagamento!!"), HttpStatus.CREATED);
+       }catch (Exception erro){
+           return new ResponseEntity<>(new ResponseDto(erro.getMessage()),HttpStatus.INTERNAL_SERVER_ERROR);
+       }
+   }
+   // adicionar o data e o horário para a proxima spring
+}
