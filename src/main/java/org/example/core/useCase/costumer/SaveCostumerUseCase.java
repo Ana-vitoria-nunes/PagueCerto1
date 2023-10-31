502094275d79ac6845d0dbb523d967ffd0020321
Ana.Vitoria.Nunes <ana.nunes@zup.com.br>
2023-10-31 14:57:24 -0300

    Subindo class de teste do address e do card

--- src/main/java/org/example/core/useCase/costumer/SaveCostumerUseCase.java
+++ src/main/java/org/example/core/useCase/costumer/SaveCostumerUseCase.java
@@ -5,27 +5,16 @@ import org.example.adapters.config.Pass;
 import org.example.core.domain.model.Costumer;
 import org.example.core.domain.model.dto.requestDto.CostumerRequest;
 import org.example.core.port.CostumerRepository;
-import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class SaveCostumerUseCase {
-    //  private final PasswordEncoderServiceRepository passwordEncoderService;
-    // private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-//    public void generateAndSetExternalId(Costumer ) {
-//        this.externalId = UUID.randomUUID().toString();
-//    }
-//    private final PasswordEncoder pass;
-
-    private final ModelMapper mapper;
     private final CostumerRepository costumerRepository;
 
     public Costumer saveCostumer(CostumerRequest dtoCliente) {
             Costumer costumer = new Costumer();
-            //  String senhaCriptografada = passwordEncoderService.encodePassword(dtoCliente.getSenha(), bCryptPasswordEncoder);
-            // costumer.setSenha(pass.encode(dtoCliente.getSenha()));
 
             String s = Pass.hashCrypto(dtoCliente.getSenha());
             dtoCliente.setSenha(s);
